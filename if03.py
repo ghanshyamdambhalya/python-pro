36563ba658d0ff60c71416cfc21b8e4512169459
ghanshyamdambhalya <117528456+ghanshyamdambhalya@users.noreply.github.com>
2022-11-18 13:35:51 +0530

    push
    push

--- if03.py
+++ if03.py
@@ -1,6 +1,5 @@
 #write a program to accept one letter from user. if letter is a then print message "it is first letter of abcd".
 letter = str(input("Enter the letter"))
-a=
-if letter==a:
+if letter=='a':
     print("It is first letter of abcd")
 print("good bye")
